997a59d1a074a3ca0c4730cc6c7cfadbf4494a32
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-06-23 16:02:11 +0300

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,12 @@ function App() {
 
   return (
     <div className='Container'>
+      <img src="/src/assets/react.svg" alt="Welcome Logo" style={{ width: 64, height: 64, marginBottom: 12, marginTop: 8 }} />
       <LanguageSelector />
       <h1>{t("greetings")}</h1>
-
+      <div style={{ color: '#6366f1', fontWeight: 500, fontSize: '1.1rem', marginBottom: 8, marginTop: -8, textAlign: 'center' }}>
+        {t('subtitle', 'Your multilingual experience starts here!')}
+      </div>
       <Trans
         i18nKey={line1}
         values={{ author: "Ridhwan Athaullah" }}
@@ -18,8 +21,6 @@ function App() {
           1: <b /> //basically bold
         }}
         />
-
-      
       <p>{line2}</p>
     </div>
   )
